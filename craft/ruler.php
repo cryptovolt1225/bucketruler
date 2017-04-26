49d65df7f0b4f309497ecd809b136f3d1014198f
Eder John <ejscheid@inf.ufrgs.br>
2017-04-26 14:21:39 -0300

    Diversas alterações

--- craft/ruler.php
+++ craft/ruler.php
@@ -1,7 +1,9 @@
 <?php
 require('fpdf/fpdf.php');
 // Check for empty fields
-if(empty($_POST['diameter'])  		||
+$diameter = 110;
+$height = 10;
+/*if(empty($_POST['diameter'])  		||
    empty($_POST['height']) 		||
    )
    {
@@ -9,10 +11,8 @@ if(empty($_POST['diameter'])  		||
 	  return false;
    }
 	
-$diameter = strip_tags(htmlspecialchars($_POST['diamter']));
-$height = strip_tags(htmlspecialchars($_POST['height']));
-	
-
+$diameter = $_POST['diameter'];
+$height = $_POST['height'];*/
 
 $pdf = new FPDF( 'P', 'mm', 'A4' );
 
@@ -21,11 +21,11 @@ $pdf->SetDisplayMode(real,'default');
 $pdf->SetFillColor(0,0,0);
 $pdf->SetFont('Arial','B',16);
 
-$pdf->Image('logo.jpg',20,10,50,33.3);
-
-$pdf->SetDrawColor(188,188,188);
+$pdf->SetDrawColor(0,0,0);
 $pdf->Line(20, 45, 210-$diameter, 45); // 20mm from each edge
 $pdf->Line(50, 45, 210-$height, 45); // 50mm from each edge$pdf->Line(20,45,150,45);
-
-return true;			
+//$pdf->Output("mypdf.pdf","S");
+return $str = iconv('UTF-8', 'windows-1252', $pdf->Output("mypdf.pdf","I"));
+//echo $pdf->Output("mypdf.pdf","D");
+//echo $content;
 ?>
--- js/craftRuler.js
+++ js/craftRuler.js
@@ -4,6 +4,7 @@ $(function() {
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
+
         },
         submitSuccess: function($form, event) {
             // Prevent spam click and default submit behaviour
@@ -11,18 +12,56 @@ $(function() {
             event.preventDefault();
             
             // get values from FORM
-            var name = $("input#diameter").val();
-            var email = $("input#height").val();
+            var diameter = $("input#diameter").val();
+            var height = $("input#height").val();
+			var r = diameter/2.0;
+			var pi = Math.PI;
+			var margin = 0.635;
+			var ruler_width = 5;
+			var litersPerCm = pi * (r^2);
+			var height2 = 0;
 
-            $.ajax({
-                url: "././craft/ruler.php",
+			if (height > 29.7) {
+				height2 = height - 29.7;
+			}
+
+			var doc = new jsPDF({
+			  unit: 'cm',
+			  format: 'a4'
+			});
+			doc.setProperties({
+			    title: 'Bucket Ruler',
+			    author: 'bucketRuler',
+			    keywords: 'generated, javascript, web 2.0, ajax',
+			});
+
+			//doc.line(0.635, 0.635, 0.635, height);
+			//doc.line(0.635, 2, 0.635, 0.635);
+			
+			doc.rect(margin, margin, ruler_width, height - (margin*2)); // empty square
+			var i = 0;
+			while(i<=height-(margin*2))
+			{
+				if (i!=0){
+					doc.line(ruler_width, i+margin, margin+ruler_width, i+margin);
+					doc.text(ruler_width-margin*2,i+margin,((litersPerCm*i)/100).toFixed(2).toString()+'l');
+				}
+				i=i+1.5;
+			}
+			if (height2 > 0)
+			{
+				doc.rect(margin+7, margin, ruler_width, height2 - (margin*2)); // empty square
+			}
+			doc.save('ruler.pdf');
+          /* $.ajax({
+                url: "./craft/ruler.php",
                 type: "POST",
                 data: {
                     diameter: diameter,
                     height: height,
                 },
                 cache: false,
-                success: function() {
+                success: function(data) {
                     // Enable button & show success message
                     $("#btnSubmit").attr("disabled", false);
                     $('#measureSuccess').html("<div class='alert alert-success'>");
@@ -36,7 +75,7 @@ $(function() {
                     //clear all fields
                     $('#measureForm').trigger("reset");
                 },
-                error: function() {
+                error: function(data) {
                     // Fail message
                     $('#measureSuccess').html("<div class='alert alert-danger'>");
                     $('#measureSuccess > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -46,7 +85,7 @@ $(function() {
                     //clear all fields
                     $('#measureForm').trigger("reset");
                 },
-            });
+            }); */
         },
         filter: function() {
             return $(this).is(":visible");
@@ -63,3 +102,28 @@ $(function() {
 $('#diameter').focus(function() {
     $('#measureSuccess').html('');
 });
+function dottedLine(doc, xFrom, yFrom, xTo, yTo, segmentLength)
+{
+    // Calculate line length (c)
+    var a = Math.abs(xTo - xFrom);
+    var b = Math.abs(yTo - yFrom);
+    var c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+    // Make sure we have an odd number of line segments (drawn or blank)
+    // to fit it nicely
+    var fractions = c / segmentLength;
+    var adjustedSegmentLength = (Math.floor(fractions) % 2 === 0) ? (c / Math.ceil(fractions)) : (c / Math.floor(fractions));
+
+    // Calculate x, y deltas per segment
+    var deltaX = adjustedSegmentLength * (a / c);
+    var deltaY = adjustedSegmentLength * (b / c);
+
+    var curX = xFrom, curY = yFrom;
+    while (curX <= xTo && curY <= yTo)
+    {
+        doc.line(curX, curY, curX + deltaX, curY + deltaY);
+        curX += 2*deltaX;
+        curY += 2*deltaY;
+    }
+}
+
